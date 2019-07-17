97e1614f3a37c2bfb4ad905f878c8ee9329a4323
老朱 <1534547330@qq.com>
2019-07-17 11:52:00 +0800

    z2

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -7,7 +7,22 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    word:"wuhan 1902精通wxml",
+    flag:!!1,
+    count:2000
+  },
+  countAdd(){
+    this.data.count++; //只是同步修改 ，师徒不改变
+    //自增一，不必须再，setData里赋值视图才会更新
+    this.setData({  //异步修改 视图更新 ,
+      count:this.data.count+=20     //两个count一起弄，最终会 自加21
+    })
+  },
+  gotohome(){
+    wx.navigateTo({
+      url:'../home/home?id=1902'  //直接跳转
+    })
   },
   //事件处理函数
   bindViewTap: function() {
